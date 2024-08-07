ffd0fdf75c6908c635b498750b24999a0c0f4fd5
NimaySh <nimaysharma101@gmail.com>
2024-08-07 12:22:38 -0400

    Template for React and Tailwind Use

---
+++ hsinternsorg/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <head>
+      <div>
+        <h1>Hello Andrew, page is kinda like the index.html file for react and tailwind</h1>
+      </div>
+    </head>
+  );
+}
