72292e3c1c7d2a6fa259fb989ab8467f6afbc884
NimaySh <nimaysharma101@gmail.com>
2024-08-20 13:35:38 -0400

    Getting ready to deploy just refactoring for next

--- hsinternsorg/app/page.tsx
+++ hsinternsorg/app/page.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { HoverEffect } from "@/app/components/ui/card-hover-effect"; // Adjust the path if necessary
 import { FiChevronDown } from 'react-icons/fi';
 import Link from 'next/link';
- 
+import { projects } from '@/app/data/projects';
+
 export default function Home() {
   return (
     <div className="bg-gray-50 font-sans">
@@ -55,35 +56,4 @@ export default function Home() {
   );
 }
 
-export const projects = [
-  {
-    title: "1. You'll Learn.",
-    description:
-      "Our verified knowledgeable tutors are dedicated to teaching you how to think, design, and code like a web developer.",
-  },
-  {
-    title: "2. We'll Test.",
-    description:
-      "Before you work with our partnered non-profit organizations we'll make sure you can do the job through a short interview.",
-  },
-  {
-    title: "3. They'll Employ",
-    description:
-      "Once you've passed our interview we will assign you to a team and one of our tutors to gain experience by helping a non-profit.",
-  },
-  {
-    title: "4. You'll Win",
-    description:
-      "Through your time working with the organization you'll have a resume-worthy experience and skills to continue your web-development journey.",
-  },
-  {
-    title: "5. You Can Come Back",
-    description:
-      "With your skills, we can verify you as a tutor and you'll teach more students and get more experiences at non-profit organizations.",
-  },
-  {
-    title: "6. Everyone Wins",
-    description:
-      "You learn + get hands-on experience while non-profits get a free modern website.",
-  },
-];
+
